4a4a273585c7c7061774a7f0ddf1443677ddff0d
Aaron Williams <aaronrw4@yahoo.com>
2021-04-18 21:23:31 -0700

    adjusted server.js

--- server.js
+++ server.js
@@ -8,36 +8,6 @@ server.use(express.json());
 server.use(CORS());
 server.use('/api/movies', moviesRouter)
 
-server.put("/api/movies/:id", (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the movie id");
-  if (
-    req.body.id === undefined ||
-    !req.body.title ||
-    !req.body.director ||
-    !req.body.metascore ||
-    !req.body.stars
-  ) {
-    res
-      .status(422)
-      .send("Make sure your request body has all the fields it needs");
-  }
-  movies = movies.map(movie => {
-    if (`${movie.id}` === req.params.id) {
-      return req.body;
-    }
-    return movie;
-  });
-  res.status(200).send(req.body);
-});
-
-server.delete("/api/movies/:id", (req, res) => {
-  if (!req.params.id)
-    res.status(400).send("Your request is missing the movie id");
-  movies = movies.filter(movie => `${movie.id}` !== req.params.id);
-  res.status(202).send(req.params.id);
-});
-
 server.get("/", function(req, res) {
   res.send("server is working 👍");
 });
